07218abc7d03a05ea833794c758b4b8a526a74a0
k8abadzheva <make.me.loco@gmail.com>
2025-04-01 23:21:37 +0300

    Implement text persistence across activity restarts
    - Save and restore the text in the TextView across activity restarts using `onSaveInstanceState` and `onCreate`.
    - Introduce a constant `KEY_TEXT` for the saved state key.
    - Set the initial text in the TextView to "Hello World!" or the restored text if available.

--- app/src/main/java/com/abadzheva/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/com/abadzheva/zerotoheroandroidtdd/MainActivity.kt
@@ -6,14 +6,33 @@ import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var tv: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val btn = findViewById<Button>(R.id.changeButton)
-        val tv = findViewById<TextView>(R.id.titleTextView)
+        tv = findViewById<TextView>(R.id.titleTextView)
+
+        tv.text = savedInstanceState?.getString(KEY_TEXT) ?: getString(
+            R
+                .string
+                .hello_world
+        )
+
         btn.setOnClickListener {
             tv.text = getString(R.string.i_am_an_android_developer)
         }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString(KEY_TEXT, tv.text.toString())
+    }
+
+
+    companion object {
+        private const val KEY_TEXT = "text_key"
     }
 }
